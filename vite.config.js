e6cf2f651b19f1de572d17b6356eed3bca797264
Ofemibabz <ofemi.b@gmail.com>
2026-08-04 21:40:10 -0600

    Create your-health and patient-policies pages tailored to Lundys Lane

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         dr_b: resolve(__dirname, 'dr-b.html'),
         dr_deji: resolve(__dirname, 'dr-deji.html'),
+        your_health: resolve(__dirname, 'your-health.html'),
+        patient_policies: resolve(__dirname, 'patient-policies.html'),
       },
     },
   },
