feff8c53582be503c75cf9cdc92fee1581caa3f4
Ofemibabz <ofemi.b@gmail.com>
2026-08-10 14:56:22 -0600

    Update team to 3 physicians (Dr. B, Dr. Bayo, Dr. Jane Doe), update opening hours, add registration dropdown & address autocomplete, and implement interactive single-section tab filtering on your-health.html

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         dr_b: resolve(__dirname, 'dr-b.html'),
-        dr_deji: resolve(__dirname, 'dr-deji.html'),
+        dr_bayo: resolve(__dirname, 'dr-bayo.html'),
+        dr_jane: resolve(__dirname, 'dr-jane.html'),
         your_health: resolve(__dirname, 'your-health.html'),
         patient_policies: resolve(__dirname, 'patient-policies.html'),
         register: resolve(__dirname, 'register.html'),
